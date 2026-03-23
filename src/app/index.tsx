506dbd1300ca2c68a62a5375a9cc952436b1180d
José de Ribamar <jribamarcl@hotmail.com>
2026-03-23 19:12:27 -0300

    atualizado

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View  className="flex-1 content-center justify-center snap-align-none">
-      <Text>OLÁ MUNDO TODO.</Text>
+      <Text>OLÁ MUNDO!.</Text>
     </View>
   );
 }
